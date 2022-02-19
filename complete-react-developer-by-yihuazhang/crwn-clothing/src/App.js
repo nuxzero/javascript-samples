6e4f0b2164731fe59db34d3b651e20ef3d08ef24
Natthawut Haematulin <natthawut1991@gmail.com>
2022-02-19 11:37:52 +0700

    feat: create collection preview, collection item components

--- complete-react-developer-by-yihuazhang/crwn-clothing/src/App.js
+++ complete-react-developer-by-yihuazhang/crwn-clothing/src/App.js
@@ -1,15 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopePage from "./pages/shop/shop.component";
 
 import "./App.css";
-
-const HatesPage = () => <div>Hats page</div>;
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<HomePage />} />
-      <Route path="/hats" element={<HatesPage />} />
+      <Route path="/shop" element={<ShopePage />} />
     </Routes>
   );
 }
---
+++ complete-react-developer-by-yihuazhang/crwn-clothing/src/pages/shop/shop.component.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import SHOP_DATA from "./shop.data";
+
+export class ShopePage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shope-page">
+        {collections.map(({ id, ...props }) => (
+          <CollectionPreview key={id} {...props} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopePage;
