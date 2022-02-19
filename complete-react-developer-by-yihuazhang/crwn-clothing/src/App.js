f26a280bb02620d32ffc4470924fb1738ebd7091
Natthawut Haematulin <natthawut1991@gmail.com>
2022-02-19 15:37:48 +0700

    feat: create sign-in page

--- complete-react-developer-by-yihuazhang/crwn-clothing/src/App.js
+++ complete-react-developer-by-yihuazhang/crwn-clothing/src/App.js
@@ -1,9 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopePage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInAndSignOutPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 import "./App.css";
-import Header from "./components/header/header.component";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopePage />} />
+        <Route path="/signin" element={<SignInAndSignOutPage />} />
       </Routes>
     </>
   );
