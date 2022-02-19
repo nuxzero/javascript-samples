8d085c66f30e17780086aaeb2f729aead2aa1482
Natthawut Haematulin <natthawut1991@gmail.com>
2022-02-19 13:43:48 +0700

    feat: create header component

--- complete-react-developer-by-yihuazhang/crwn-clothing/src/App.js
+++ complete-react-developer-by-yihuazhang/crwn-clothing/src/App.js
@@ -3,13 +3,17 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopePage from "./pages/shop/shop.component";
 
 import "./App.css";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
-    <Routes>
-      <Route exact path="/" element={<HomePage />} />
-      <Route path="/shop" element={<ShopePage />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopePage />} />
+      </Routes>
+    </>
   );
 }
 
