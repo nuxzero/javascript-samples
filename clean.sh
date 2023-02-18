0486d5877eef32c708701b415aedd72578a8cd91
Natthawut Haematulin <natthawut1991@gmail.com>
2023-02-18 23:09:05 +0700

    feat: add sample projects

--- clean.sh
+++ clean.sh
@@ -5,3 +5,12 @@ find . -name 'node_modules' -type d -prune -print | xargs du -chs
 
 # Remove all node_modules directories
 find . -name 'node_modules' -type d -prune -print -exec rm -rf '{}' \;
+
+# Remove all dist directories
+find . -name 'dist' -type d -prune -print -exec rm -rf '{}' \;
+
+# Remove all out directories
+find . -name 'out' -type d -prune -print -exec rm -rf '{}' \;
+
+# Remove all .next directories
+find . -name '.next' -type d -prune -print -exec rm -rf '{}' \;
