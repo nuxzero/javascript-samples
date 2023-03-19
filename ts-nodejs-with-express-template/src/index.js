dcf61c31431bba2b1525e2e54232da9debb6f866
Natthawut Haematulin <natthawut1991@gmail.com>
2023-03-19 16:07:27 +0700

    feat: implement sample api

--- ts-nodejs-with-express-template/src/index.js
+++
@@ -1,3 +0,0 @@
-import * as fs from "fs";
-console.log("hello world!");
-fs.readFile("src/index.ts", (err, data) => { });
--- ts-nodejs-with-express-template/src/index.ts
+++ ts-nodejs-with-express-template/src/index.ts
@@ -1,1 +1,22 @@
-console.log("hello world");
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+
+const API_PORT = 8080;
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World!" });
+});
+
+app.post("/create", (req, res) => {
+  res.json({ message: "Hello World!" });
+});
+
+app.listen(API_PORT, () => {
+  console.log(`Listening on port ${API_PORT}`);
+});
